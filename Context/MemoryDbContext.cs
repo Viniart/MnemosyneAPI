9e3775842a9dd9fe95a99ea557ae84ae1857cbcd
Vinicio S <vinicio.santos@sp.senai.br>
2025-02-25 19:55:39 -0300

    Adicionar arquivos de projeto.

---
+++ Context/MemoryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MnemosyneAPI.Model;
+
+namespace MnemosyneAPI.Context
+{
+    public class MemoryDbContext : DbContext
+    {
+        public MemoryDbContext(DbContextOptions<MemoryDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Memory> Memories => Set<Memory>();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MnemosyneAPI.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Mnemosyne API",
+        Version = "v1",
+        Description = "API desenvolvida no curso de Programação com C#, para atender ao Frontend do site Mnemosyne"
+    });
+});
+
+builder.Services.AddDbContext<MemoryDbContext>(options => options.UseSqlite("Data Source=memories.db"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mnemosyne API v1");
+    });
+}
+
+app.Run();
